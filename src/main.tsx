91558adfe52723c53527ff235d0b1306ea10facb
BennyTheMemer <bernardo_brg@hotmail.com>
2022-04-12 15:28:00 +0100

    router, react-query, sass, jest, redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLDivElement
+);
+root.render(<App />);
